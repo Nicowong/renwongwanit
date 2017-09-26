655e1f6ae14bf6aeecd2c80b37354bfa65723be1
nicolas wongwanit <nicolas.wongwanit@gmail.com>
2017-09-26 13:54:32 +0200

    Fichier main.cpp reparé, récupère les fichier src/shared/state.h et Exemple.h

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -20,7 +20,6 @@ int main(int argc,char* argv[])
     exemple.setX(53);
 
     cout << "It works !" << endl;
-<<<<<<< HEAD
     cout << "Bonjour le monde ! src/client/main.cpp" << endl;
     
     /*-----------------TEST SFML----------------*/
@@ -55,8 +54,6 @@ int main(int argc,char* argv[])
         
         sf::sleep(sf::seconds(1.0f));
     }
-=======
->>>>>>> parent of 0d24730... compilation reussie
 
     return 0;
 }
