bbcd1176b71468f5cf74426a18257afda2fe35bb
nicolas wongwanit <nicolas.wongwanit@gmail.com>
2017-09-26 12:01:30 +0200

    Test SFML

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -21,6 +21,39 @@ int main(int argc,char* argv[])
 
     cout << "It works !" << endl;
     cout << "Bonjour le monde ! src/client/main.cpp" << endl;
+    
+    /*-----------------TEST SFML----------------*/
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shapeG(100.f);
+    shapeG.setFillColor(sf::Color::Green);
+    
+    sf::CircleShape shapeR(100.f);
+    shapeR.setFillColor(sf::Color::Red);
+    
+    int i = 0 ;
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        if(i == 0){
+            window.draw(shapeG);
+            i=1;
+        }else{
+            window.draw(shapeR);
+            i=0;
+        }
+        window.display();
+        
+        sf::sleep(sf::seconds(1.0f));
+    }
 
     return 0;
 }
