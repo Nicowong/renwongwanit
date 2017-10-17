0a51519aea3b5e933db82fe6311d11d662af8582
REN <ren39376659@gmail.com>
2017-10-17 09:51:09 +0200

    main.cpp change

---
+++ CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/src/client/Unit.cpp.o"
+  "CMakeFiles/client.dir/src/client/Cell.cpp.o"
+  "CMakeFiles/client.dir/src/client/Building.cpp.o"
+  "CMakeFiles/client.dir/src/client/main.cpp.o"
+  "CMakeFiles/client.dir/src/client/Player.cpp.o"
+  "bin/client.pdb"
+  "bin/client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
