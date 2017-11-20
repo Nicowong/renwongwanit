23385778bc50cf359c66863eb804279f9392210c
ren <ren39376659@gmai.com>
2017-11-20 17:13:21 +0100

    ai

--- src/shared/ai/Ai.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Ai.h"
-
-using namespace std ;
-using namespace ai ;
-
-void Ai::listCommands (std::vector<engine::Command>& list, const state::State state, state::Element& selected){
-
-}
